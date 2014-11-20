1b7341bb7443b3c95c7a2c5da9ad65bca0912555
lucas-ez <lucas@midokura.com>
2014-11-20 16:53:18 +0000

    updated list of tempest tests

--- scripts/ci/thirdparty/run_tempest.sh
+++ scripts/ci/thirdparty/run_tempest.sh
@@ -18,8 +18,7 @@
 sed -ri 's/ipv6_subnet_attributes = True/ipv6_subnet_attributes = False/g' /opt/stack/tempest/etc/tempest.conf
 sed -ri 's/ipv6 = True/ipv6 = False/g' /opt/stack/tempest/etc/tempest.conf
 
-PYTHONPATH=/opt/stack/tempest nosetests -vv tempest.api.network.admin.test_agent_management \
-tempest.api.network.admin.test_external_network_extension \
+PYTHONPATH=/opt/stack/tempest nosetests -vv \
 tempest.api.network.test_networks \
 tempest.api.network.test_ports \
 tempest.api.network.test_networks_negative \
